e9df157a975ed8c01e96f350dc755acc0d1108a4
navas <muhammednavas320203@gmail.com>
2025-01-30 23:19:31 +0530

    fix: yt rag unwanted comments removed

--- youtube_rag/youtube_rag.py
+++ youtube_rag/youtube_rag.py
@@ -7,38 +7,6 @@ import os
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-# from langchain.document_loaders.parsers import
-# from langchain_community.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
-
-# import Whisper
-
-# class LocalWhisperParser(Parser):
-#     def _init_(self, model_name:str = "base"):
-#         self.model = Whisper.load_model(model_name)
-
-#     def parse(self, file_path: str) -> str:
-#         result = self.model.transcribe(file_path)
-#         return result["text"]
-
-# url = "https://youtu.be/1Mx2VhJaN1U?si=qXh7e5FMfVOJfZRX"
-# save_dir = "docs/youtube/"
-
-
-# docs = GenericLoader(
-#     YoutubeAudioLoader([url], save_dir),
-
-# )
-
-
-# loader = YoutubeLoader.from_youtube_url(
-#     "https://www.youtube.com/watch?v=2sdXSczmvNc", add_video_info=False
-# )
-
-
-# docs = loader.load()
-
-# print("Youtube Docs", docs[0].page_content[:100])
-
 
 
 def extract_text_from_youtube_url(url):
@@ -51,12 +19,6 @@ def summarize_text_groq(text, model_name="gemma2-9b-it", temperature=0.7):
         return "Error: No text provided"
 
     llm = ChatGroq(model_name=model_name, temperature=temperature)
-
-    # # Summarization prompt
-    # prompt = ( ":\n\n"
-    #            f"{text}\n\n"
-    #             "."
-    #         )
 
       # Prepare messages
     messages = [
@@ -75,7 +37,7 @@ def summarize_text_groq(text, model_name="gemma2-9b-it", temperature=0.7):
 # Main Function
 
 if __name__ == "__main__":
-    youtube_url = "https://www.youtube.com/watch?v=2sdXSczmvNc"
+    youtube_url = "https://youtu.be/K6G7E6vCDw0?si=q31j0i6ZtdsK6Nzz"
     extracted_text = extract_text_from_youtube_url(youtube_url)
     print("Extracted Text:\n", extracted_text[0].page_content[:20])  # Print a snippet of the text
 
